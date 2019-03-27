09d95ca179dfb1aab20d4ec28c1d3db49a281cf3
Bruno Celaya Montero <brunozelaia@gmail.com>
2019-03-27 11:45:07 +0100

    Update leermasdin.js

--- leermasdin.js
+++ leermasdin.js
@@ -3,11 +3,11 @@ var texto = document.getElementsByClassName("term-description")[0].innerHTML;
 var longtexto = texto.length;
 var primerp = texto.indexOf("</p>");
 if (longtexto != (primerp+5))
-	{
-	var primeraparte = texto.slice(0,(primerp+4));
-	var segundaparte = texto.slice((primerp+4));
-	var segundaparte = "<div id='leermas'>" + segundaparte + "</div>"
-	var union = primeraparte + "<br/><a class='leermasjs' onclick='leermas()'>Leer más...</a>" + segundaparte;
-	document.getElementsByClassName("term-description")[0].innerHTML=union;
-	}
+    {
+    var primeraparte = texto.slice(0,(primerp+4));
+    var segundaparte = texto.slice((primerp+4));
+    var segundaparte = "<div id='leermas'>" + segundaparte + "</div>"
+    var union = primeraparte + "<br/><a class='leermasjs' onclick='leermas()'>Leer más...</a>" + segundaparte;
+    document.getElementsByClassName("term-description")[0].innerHTML=union; var nodoleermas = document.getElementById("leermas").style.display="none";
+    }
 }
